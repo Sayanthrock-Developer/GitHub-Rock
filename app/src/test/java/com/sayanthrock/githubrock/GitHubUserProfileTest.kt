a02b68d3c061dc35f431bf404616b02dfead9d28
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-07-14 09:59:20 +0530

    test: verify unused GitHub social fields are ignored

---
+++ app/src/test/java/com/sayanthrock/githubrock/GitHubUserProfileTest.kt
@@ -0,0 +1,26 @@
+package com.sayanthrock.githubrock
+
+import com.sayanthrock.githubrock.core.model.GitHubUser
+import kotlinx.serialization.json.Json
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GitHubUserProfileTest {
+    @Test fun profileIgnoresUnusedSocialFields() {
+        val user = Json { ignoreUnknownKeys = true }.decodeFromString<GitHubUser>(
+            """
+            {
+              "login": "SayanthRock",
+              "id": 202829406,
+              "avatar_url": "https://avatars.githubusercontent.com/u/202829406",
+              "public_repos": 24,
+              "followers": 120,
+              "following": 73
+            }
+            """.trimIndent()
+        )
+
+        assertEquals("SayanthRock", user.login)
+        assertEquals(24, user.publicRepos)
+    }
+}
