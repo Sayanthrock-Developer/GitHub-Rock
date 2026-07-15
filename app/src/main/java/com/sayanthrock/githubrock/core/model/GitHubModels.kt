d8d0827416891bc289a3c4d5e108a046c34f9d99
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-07-15 17:26:01 +0530

    Expose GitHub profile location and website

--- app/src/main/java/com/sayanthrock/githubrock/core/model/GitHubModels.kt
+++ app/src/main/java/com/sayanthrock/githubrock/core/model/GitHubModels.kt
@@ -11,6 +11,8 @@ data class GitHubUser(
     @SerialName("avatar_url") val avatarUrl: String = "",
     val name: String? = null,
     val bio: String? = null,
+    val location: String? = null,
+    val blog: String? = null,
     @SerialName("public_repos") val publicRepos: Int = 0,
     val followers: Int = 0,
     val following: Int = 0
