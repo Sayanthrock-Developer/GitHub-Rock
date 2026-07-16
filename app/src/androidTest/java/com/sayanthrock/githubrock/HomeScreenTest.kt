396da478dce764072a8497ec32a79698f3273c1e
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-07-16 16:16:47 +0530

    Fix Compose loading-state test import

--- app/src/androidTest/java/com/sayanthrock/githubrock/HomeScreenTest.kt
+++ app/src/androidTest/java/com/sayanthrock/githubrock/HomeScreenTest.kt
@@ -1,7 +1,6 @@
 package com.sayanthrock.githubrock
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
