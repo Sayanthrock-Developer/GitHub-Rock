963fe18e7eb39a06c55cee4c44a3e2236c756012
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-09-09 23:03:57 +0530

    fix: use configured JUnit for blur tests

--- app/src/test/java/com/sayanthrock/githubrock/ui/blur/ApplicationBlurTest.kt
+++ app/src/test/java/com/sayanthrock/githubrock/ui/blur/ApplicationBlurTest.kt
@@ -1,9 +1,9 @@
 package com.sayanthrock.githubrock.ui.blur
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class ApplicationBlurTest {
     @Test
