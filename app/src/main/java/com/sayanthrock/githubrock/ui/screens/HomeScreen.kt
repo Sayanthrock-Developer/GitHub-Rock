4fbbaa4bb77079c94f77b0f61e66a1d86af435c5
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-09-21 21:50:34 +0530

    remove remaining dashboard watch references

--- app/src/main/java/com/sayanthrock/githubrock/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/sayanthrock/githubrock/ui/screens/HomeScreen.kt
@@ -242,8 +242,6 @@ fun HomeScreen(
                     } else {
                         null
                     },
-                    watchEnabled = watchEnabled,
-                    onWatch = onWatch
                     onClick = { onOpenRepo(repository) },
                 )
             }
