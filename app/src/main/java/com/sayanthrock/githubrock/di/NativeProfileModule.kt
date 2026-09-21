8624f83efba1292b7dae84a819986ef1e18131d2
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-09-21 07:55:56 +0530

    refactor(network): share GitHub Retrofit with profile API

--- app/src/main/java/com/sayanthrock/githubrock/di/NativeProfileModule.kt
+++ app/src/main/java/com/sayanthrock/githubrock/di/NativeProfileModule.kt
@@ -1,26 +1,18 @@
 package com.sayanthrock.githubrock.di
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sayanthrock.githubrock.core.network.GitHubProfileApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NativeProfileModule {
     @Provides
     @Singleton
-    fun nativeProfileApi(json: Json, client: OkHttpClient): GitHubProfileApi = Retrofit.Builder()
-        .baseUrl("https://api.github.com/")
-        .client(client)
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .build()
-        .create(GitHubProfileApi::class.java)
+    fun nativeProfileApi(retrofit: Retrofit): GitHubProfileApi =
+        retrofit.create(GitHubProfileApi::class.java)
 }
