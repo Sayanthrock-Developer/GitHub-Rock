270880fccd8144eee0c07cadd2765d12f16c1642
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-09-23 12:08:32 +0530

    fix: restore navigation width import

--- app/src/main/java/com/sayanthrock/githubrock/ui/navigation/RockNavigationChrome.kt
+++ app/src/main/java/com/sayanthrock/githubrock/ui/navigation/RockNavigationChrome.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
